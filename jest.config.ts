60d308f5c95e474cce081067cf8679f00bc319af
lucas <santanafx@hotmail.com>
2025-07-21 19:40:52 -0300

    feat: Enhance Jest configuration with TypeScript support and module file extensions

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,15 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+import type { Config } from "jest";
 
 const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
