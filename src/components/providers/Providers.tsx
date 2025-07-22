b5e4c93a4d3c21ab79be30886534f910939c7676
lucas <santanafx@hotmail.com>
2025-07-22 19:20:39 -0300

    feat: Wrap children in AuthProvider within QueryClientProvider for authentication context integration

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -4,11 +4,14 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import { ProvidersProps } from "./Providers.type";
+import { AuthProvider } from "@/context/auth-context";
 
 export default function Providers({ children }: ProvidersProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>{children}</AuthProvider>
+    </QueryClientProvider>
   );
 }
