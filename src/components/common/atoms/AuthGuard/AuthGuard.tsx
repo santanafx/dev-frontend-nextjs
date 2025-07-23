309a107d3c5413370d60bb77272cb825846e03cb
lucas <santanafx@hotmail.com>
2025-07-23 10:21:44 -0300

    feat: Add AuthGuard component for user authentication and loading state management

---
+++ src/components/common/atoms/AuthGuard/AuthGuard.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import type React from "react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useAuth } from "@/context/auth-context";
+import { AuthGuardProps } from "./AuthGuard.types";
+import Loader from "../Loader/Loader";
+
+export function AuthGuard({ children }: AuthGuardProps) {
+  const { user, isLoading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.push("/login");
+    }
+  }, [user, isLoading, router]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (!user) {
+    return null;
+  }
+
+  return <>{children}</>;
+}
