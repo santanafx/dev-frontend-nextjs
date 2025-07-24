8c5398baf998351424bcca05d29abb41a5ff13f9
lucas <santanafx@hotmail.com>
2025-07-24 09:49:55 -0300

    feat: Enhance Loader component to accept dynamic loading text via props

--- src/components/common/atoms/Loader/Loader.tsx
+++ src/components/common/atoms/Loader/Loader.tsx
@@ -1,11 +1,12 @@
 import { Loader2 } from "lucide-react";
+import { LoaderProps } from "./Loader.types";
 
-const Loader = () => {
+const Loader = ({ text }: LoaderProps) => {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="text-center">
         <Loader2 className="h-8 w-8 animate-spin mx-auto mb-4" />
-        <p className="text-muted-foreground">Carregando...</p>
+        <p className="text-muted-foreground">{text}</p>
       </div>
     </div>
   );
