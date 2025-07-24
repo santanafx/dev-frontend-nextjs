b02af6d4d0f235178c857f9b5886e94ef3cc3c90
lucas <santanafx@hotmail.com>
2025-07-23 21:33:45 -0300

    feat: Implement useGetProduct hook for fetching product details with error handling

---
+++ src/features/Products/hooks/useGetProduct/useGetProduct.ts
@@ -0,0 +1,21 @@
+import { apiService } from "@/service/api/config";
+import { QueryKeys } from "@/service/api/queryKeys";
+import { useQuery } from "@tanstack/react-query";
+import { Product } from "../../types/product.types";
+import { UseGetProductProps } from "./useGetProduct.types";
+import { toast } from "sonner";
+
+export function useGetProduct({ id }: UseGetProductProps) {
+  const query = useQuery<Product>({
+    queryKey: [QueryKeys.PRODUCT],
+    queryFn: () => apiService.get<Product>(`/products/${id}`)
+  });
+
+  if (query.error) {
+    const errorMessage =
+      (query.error as any).response?.data?.message || "Erro ao buscar produto!";
+    toast.error(errorMessage);
+  }
+
+  return query;
+}
