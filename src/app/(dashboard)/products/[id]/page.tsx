ae9729636eba465459b941c0b57dce63545c4b38
lucas <santanafx@hotmail.com>
2025-07-23 21:32:27 -0300

    feat: Create ProductPage component to render ProductContainer for product details display

---
+++ src/app/(dashboard)/products/[id]/page.tsx
@@ -0,0 +1,8 @@
+import ProductContainer from "@/features/Products/ProductContainer/ProductContainer";
+import React from "react";
+
+const ProductPage = () => {
+  return <ProductContainer />;
+};
+
+export default ProductPage;
